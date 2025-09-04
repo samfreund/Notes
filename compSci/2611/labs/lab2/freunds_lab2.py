9aa07410b444322e658e2719634323ba5deacb21
samfreund <samf.236@proton.me>
2025-09-04 16:07:31 -0500

    CSC: move lab 1

---
+++ compSci/2611/labs/lab2/freunds_lab2.py
@@ -0,0 +1,13 @@
+def optimize_gene(gene_id, fasta_file_path, codon_freq_table_file_path):
+    pass
+
+def deoptimize_gene(gene_id, fasta_file_path, codon_freq_table_file_path):
+    pass
+
+def menu():
+    pass
+
+"""Note - the following lines will call your menu() function if run from this file,
+but will not run your menu if the file is imported (e.g., into the tests)"""
+if __name__ == "main":
+    menu()
